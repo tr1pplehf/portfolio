6b1e4e1ab6731e3fd77fbc42051ca521a3083d71
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-23 14:37:58 +0700

    Portfolio: Projects section.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ import HeroOrbit from "@/components/HeroOrbit";
 export const Hero = () => {
   return (
     <section className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] pointer-events-none">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{
