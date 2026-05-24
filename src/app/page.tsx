91ec8fa1c1c3b6eb644e09166e6241e4f94c687a
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-25 06:09:10 +0700

    Portfolio: Contact banner.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import {Projects} from "@/sections/Projects";
 import {Tape} from "@/sections/Tape";
 import {Testimonials} from "@/sections/Testimonials";
 import {About} from "@/sections/About";
+import {Contact} from "@/sections/Contact";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <Hero />
-      <Projects />
-      <Tape />
-      <Testimonials/>
-      <About />
-    </div>
+      <main>
+        <Hero />
+        <Projects />
+        <Tape />
+        <Testimonials/>
+        <About />
+        <Contact />
+      </main>
+    </>
   );
 }
