8c62ba1a68f34273cdfff8c2808b7c44f7049fea
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-26 05:37:12 +0700

    Portfolio: Tape анимация.

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from '@/assets/icons/star.svg'
+import {Fragment} from 'react'
 
 const words = [
   'Performant',
@@ -19,17 +20,21 @@ export const Tape = () => {
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map(word => (
-              <div
-                className="inline-flex gap-4 items-center"
-                key={word}
-              >
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:25s]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map(word => (
+                  <div
+                    className="inline-flex gap-4 items-center"
+                    key={word}
+                  >
                 <span className="text-gray-900 uppercase font-extrabold text-sm">
                   {word}
                 </span>
-                <StarIcon className="size-6 text-gray-900 -rotate-12" />
-              </div>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
