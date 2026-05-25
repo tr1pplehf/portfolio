072c406db29cd86cacd13ef785882ce096a6f8cc
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-25 15:31:27 +0700

    Portfolio: Hero section анимация.

--- src/components/HeroOrbit/HeroOrbit.tsx
+++ src/components/HeroOrbit/HeroOrbit.tsx
@@ -1,24 +1,52 @@
 import {PropsWithChildren} from "react";
+import {twMerge} from "tailwind-merge";
 
 const HeroOrbit = ({
-  children,
-  size,
-  rotation
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+                     children,
+                     size,
+                     rotation,
+                     shouldOrbit = false,
+                     shouldSpin = false,
+                     spinDuration,
+                     orbitDuration,
+                   }: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+  orbitDuration?: string;
+}>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="flex items-start justify-start" style={{
-        transform: `rotate(${rotation}deg)`,
-        width: `${size}px`,
-        height: `${size}px`
-      }}>
+      <div
+        className={twMerge(shouldOrbit && "animate-spin")}
+        style={{
+          animationDuration: orbitDuration
+        }}
+      >
         <div
-          className="inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            transform: `rotate(${rotation}deg)`,
+            width: `${size}px`,
+            height: `${size}px`
           }}
         >
-          {children}
+          <div className={twMerge(shouldSpin && "animate-spin")}
+            style={{
+              animationDuration: spinDuration
+            }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -23,60 +23,94 @@ export const Hero = () => {
         <HeroOrbit
           size={800}
           rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="6s"
         >
           <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
         <HeroOrbit
           size={550}
           rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="6s"
         >
           <StarIcon className="size-14 text-emerald-300" />
         </HeroOrbit>
         <HeroOrbit
           size={590}
           rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="6s"
         >
           <StarIcon className="size-8 text-emerald-300" />
         </HeroOrbit>
         <HeroOrbit
           size={430}
           rotation={-14}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="3s"
         >
           <SparkleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={440}
           rotation={79}
+          shouldOrbit
+          orbitDuration="32s"
+          shouldSpin
+          spinDuration="3s"
         >
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={530}
           rotation={178}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="3s"
         >
           <SparkleIcon className="size-10 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={710}
           rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="3s"
         >
           <SparkleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={720}
           rotation={85}
+          shouldOrbit
+          orbitDuration="46s"
         >
           <div className="size-3 rounded-full bg-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={520}
           rotation={-41}
+          shouldOrbit
+          orbitDuration="34s"
         >
           <div className="size-2 rounded-full bg-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={650}
           rotation={-5}
+          shouldOrbit
+          orbitDuration="42s"
         >
           <div className="size-2 rounded-full bg-emerald-300/20" />
         </HeroOrbit>
@@ -89,7 +123,9 @@ export const Hero = () => {
             alt="Person peeking from behind laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <span className="bg-green-500 size-2.5 rounded-full"></span>
+            <div className="bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
+            </div>
             <p className="text-sm font-medium">Available for new projects</p>
           </div>
         </div>
